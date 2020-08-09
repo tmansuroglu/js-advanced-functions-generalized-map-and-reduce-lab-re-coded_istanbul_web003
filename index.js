e9428e2ec4fe9f63787a5ff8cf92d91510ee68c9
Tarkan Mansuroğlu <tarkanmansuroglu@hotmail.com>
2020-08-09 16:05:10 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your functions here
+function map(array,func) {
+    return array.map(func)
+}
+
+
+function reduce(sourceArray,func,startingPoint){
+    if (startingPoint===undefined){
+        return sourceArray.reduce(func)
+    }
+    else{return sourceArray.reduce(func,startingPoint)}
+}
